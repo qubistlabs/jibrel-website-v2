e0f346f07349a1096fb5824fd18c6b7a659b362b
Dragonesis <dragonesis@ya.ru>
2018-07-02 10:36:18 +0300

    create PartnersCard component

---
+++ src/components/PartnersCard/data/data.js
@@ -0,0 +1,29 @@
+PartnersCard: [
+    {
+        img: '/assets/img/general/partners/partner-1.png'
+    },
+    {
+        img: '/assets/img/general/partners/partner-2.png'
+    },
+    {
+        img: '/assets/img/general/partners/partner-3.png'
+    },
+    {
+        img: '/assets/img/general/partners/partner-4.png'
+    },
+    {
+        img: '/assets/img/general/partners/partner-5.png'
+    },
+    {
+        img: '/assets/img/general/partners/partner-6.png'
+    },
+    {
+        img: '/assets/img/general/partners/partner-7.png'
+    },
+    {
+        img: '/assets/img/general/partners/partner-8.png'
+    },
+    {
+        img: '/assets/img/general/partners/partner-9.png'
+    },
+]
