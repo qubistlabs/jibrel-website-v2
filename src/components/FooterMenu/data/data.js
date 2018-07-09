bd714b84354d6cc7669b46815269ba1b3a226f9a
Dragonesis <dragonesis@ya.ru>
2018-07-09 19:03:11 +0300

    Added compontents AppLinks and FooterMenu

---
+++ src/components/FooterMenu/data/data.js
@@ -0,0 +1,73 @@
+FooterMenu: [
+  {
+    title: "Company",
+    links: [
+      {
+        link: "#",
+        text: "About Jibrel"
+      },
+      {
+        link: "#",
+        text: "Press & Media"
+      },
+      {
+        link: "#",
+        text: "Careers"
+      },
+    ]
+  },
+  {
+    title: "Products",
+    links: [
+      {
+        link: "#",
+        text: "About Jibrel"
+      },
+      {
+        link: "#",
+        text: "Press & Media"
+      },
+      {
+        link: "#",
+        text: "Careers"
+      },
+    ]
+  },
+  {
+    title: "Enterprise",
+    links: [
+      {
+        link: "#",
+        text: "Jibrel Search"
+      },
+      {
+        link: "#",
+        text: "Jibrel Clear"
+      },
+      {
+        link: "#",
+        text: "Jibrel Core"
+      },
+    ]
+  },
+  {
+    title: "Social",
+    links: [
+      {
+        link: "#",
+        modClass: "-box-inline",
+        socialIcon: "<svg class='icon' width='24' height='24'><use xlink:href='assets/img/svg-symbols.svg#s-c-facebook'></use></svg>"
+      },
+      {
+        link: "#",
+        modClass: "-box-inline",
+        socialIcon: "<svg class='icon' width='24' height='24'><use xlink:href='assets/img/svg-symbols.svg#s-c-twitter'></use></svg>"
+      },
+      {
+        link: "#",
+        modClass: "-box-inline",
+        socialIcon: "<svg class='icon' width='24' height='24'><use xlink:href='assets/img/svg-symbols.svg#s-c-linkedin'></use></svg>"
+      },
+    ]
+  },
+]
