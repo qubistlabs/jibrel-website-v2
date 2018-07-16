8a9cbf86b296d5dca45b7c1ed7f63e33bb72f541
Dragonesis <dragonesis@ya.ru>
2018-07-16 10:43:55 +0300

    Add new components

---
+++ src/components/InnerMenu/data/data.js
@@ -0,0 +1,19 @@
+InnerMenu: {
+  contacts: [
+    {
+      link: '#',
+      text: 'Media & PR Inquiries',
+      modClass: '-three -white'
+    },
+    {
+      link: '#',
+      text: 'Technical Support',
+      modClass: '-three -white'
+    },
+    {
+      link: '#',
+      text: 'Product & Sales Enquiry',
+      modClass: '-three -white'
+    },
+  ]
+}
---
+++ src/components/ProjectSlider/data/data.js
@@ -0,0 +1,88 @@
+ProjectSlider: {
+  teamSlide: [
+    {
+      img: 'avatar-1.png',
+      img2x: 'avatar-1@2x.png',
+      name: 'Yazan Barghuthi',
+      position: 'Founder & CEO — Jibrel Network',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-2.png',
+      img2x: 'avatar-2@2x.png',
+      name: 'Victor',
+      position: 'Technical Lead',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-3.png',
+      img2x: 'avatar-3@2x.png',
+      name: 'Talal',
+      position: 'BizDev Lead',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-4.png',
+      img2x: 'avatar-4@2x.png',
+      name: 'Hamzeh',
+      position: 'Operations Lead',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-5.png',
+      img2x: 'avatar-5@2x.png',
+      name: 'Nick',
+      position: 'Design Lead',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-6.png',
+      img2x: 'avatar-6@2x.png',
+      name: 'Aleksey',
+      position: 'Developer',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-7.png',
+      img2x: 'avatar-7@2x.png',
+      name: 'Ivan',
+      position: 'Developer',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-8.png',
+      img2x: 'avatar-8@2x.png',
+      name: 'Anna',
+      position: 'Public Relations',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-9.png',
+      img2x: 'avatar-9@2x.png',
+      name: 'Yuriy',
+      position: 'Developer',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-10.png',
+      img2x: 'avatar-10@2x.png',
+      name: 'Nikita',
+      position: 'Web Analyst',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-11.png',
+      img2x: 'avatar-11@2x.png',
+      name: 'Aleksey',
+      position: 'DevOps Engineer',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+    {
+      img: 'avatar-12.png',
+      img2x: 'avatar-12@2x.png',
+      name: 'Sergei',
+      position: 'UX/UI Designer',
+      descr: '“For companies looking to incorporate blockchain technology in their operating models, integration can be a challenge. Our solution allows institutions to use tokens in the back-end, capitalizing on the benefits of blockchain, without the need to build-out dedicated functionalities and competencies.”'
+    },
+  ]
+}
