36d7dc39a2aec0984512390b63767e9c8c108c0f
Dragonesis <dragonesis@ya.ru>
2018-07-21 02:16:58 +0300

    Adding popup component

---
+++ src/components/PopUp/data/data.js
@@ -0,0 +1,88 @@
+PopUp: {
+  baseForm: {
+    support: {
+      title: 'Technical Support'
+    },
+    media: {
+      title: 'Media & PR Inquiries'
+    },
+    product: {
+      title: 'Product & Sales Enquiry'
+    },
+  },
+  MainMenu: {
+    project: [
+      {
+        name: 'Jwallet',
+        descr: 'User-friendly and secure wallet to manage digital assets',
+        link: 'jwalet.html',
+        text: 'More info'
+      },
+      {
+        name: 'Jcash',
+        descr: 'Stable currencies on the Ethereum blockchain',
+        text: 'Coming soon'
+      },
+      {
+        name: 'Jnode',
+        descr: 'Enterprise-grade, easily set-up Ethereum access',
+        text: 'Coming soon'
+      },
+      {
+        name: 'Jsearch',
+        descr: 'Block Explorer with financial audit and reporting capabilities',
+        link: 'jsearch.html',
+        text: 'More info'
+      },
+      {
+        name: 'Jclear',
+        descr: 'Fully compliant KYC / AML solution for digital assets',
+        link: 'jclear.html',
+        text: 'More info'
+      },
+      {
+        name: 'Jcore',
+        descr: 'Blockchain platform developed specifically for Financial Services',
+        link: 'jcore.html',
+        text: 'More info'
+      },
+    ],
+    otherSection: [
+      {
+        name: 'Company',
+        pages: [
+          {
+            link: 'about.html',
+            name: 'About Jibrel',
+          },
+          {
+            link: 'news.html',
+            name: 'Press & Media',
+          },
+          {
+            link: 'careers.html',
+            name: 'Careers',
+          },
+        ]
+      },
+      {
+        name: 'Contact',
+        pages: [
+          {
+            link: 'products.html',
+            name: 'Product team',
+          },
+          {
+            link: '#',
+            name: 'Media team',
+          },
+          {
+            link: 'contact-us.html',
+            name: 'Support',
+          },
+        ]
+      }
+    ]
+  },
+
+}
