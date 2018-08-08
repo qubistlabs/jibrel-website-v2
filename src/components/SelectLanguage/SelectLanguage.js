914cb423a2254c0b0f2640ec106fa9c3f4d22cf1
Dragonesis <dragonesis@ya.ru>
2018-08-08 12:30:19 +0300

    Fix select language

--- src/components/SelectLanguage/SelectLanguage.js
+++ src/components/SelectLanguage/SelectLanguage.js
@@ -8,17 +8,22 @@ const languageSelected = languagesBox.querySelector('.-current')
 const languagesHide = languagesBox.querySelector('.hide')
 const languages = languagesBox.querySelectorAll('.item')
 
-const languageLength = languageSelected.getAttribute('data-language').length
-const languageUrl = window.location.pathname.slice(1, languageLength+1)
-const languageChanged = languagesBox.querySelector(`[data-language='${languageUrl}']`)
+const languageSite = document.querySelector('html').getAttribute('lang')
 
-if (languageChanged) {
-  languageSelected.classList.remove('-current')
-  languageChanged.classList.add('-current')
-  console.log(languageChanged.textContent)
-  languageCurrent.innerText = languageChanged.textContent
-}
+const languageChanged = languagesBox.querySelector(`[data-language='${languageSite}']`)
 
+languageSelected.classList.remove('-current')
+languageChanged.classList.add('-current')
+languageCurrent.innerText = languageChanged.textContent
+
+document.querySelectorAll('a').forEach((link) => {
+  if (link.getAttribute('target') === null) {
+    const href = link.getAttribute('href')
+    const prefix = languageSite === 'en' ? '/' : `/${languageSite}/`
+
+    link.setAttribute('href', prefix+href)
+  }
+})
 
 languageToggle.addEventListener('click', () => {
   languagesHide.classList.add('-active')
@@ -34,9 +39,12 @@ const selectLanguage = {
   },
   selectedLanguage(language) {
     const languageSelect = language.getAttribute('data-language')
+    const languagePuth = languageSelect === 'en' ? '/' : `/${languageSelect}/`
     const languageL = languageSelect.length
-    const path = window.location.pathname.substr(languageL+1)
-    document.location.href = `/${languageSelect}${path}`
+    const path = languageSite === 'en' ? window.location.pathname.substr(1) : window.location.pathname.substr(languageL+1)
+    const prefix = languageSelect === 'en' ? '' : `/${languageSelect}/`
+    document.location.href = prefix+path
+    console.log(prefix+path);
   },
   closeSelect() {
     languagesHide.classList.remove('-active')
