c2d45bc1d5452d0a0a7b97749514fc1026d35f90
Dragonesis <dragonesis@ya.ru>
2018-08-09 23:41:30 +0300

    Changed the data source in head/ Preview commit. Add title, description, keywords

--- src/components/ProjectHead/data/data.js
+++ src/components/ProjectHead/data/data.js
@@ -1,5 +1,7 @@
 ProjectHead: {
-    home: {
-        title: 'Home page',
+    BiggestWaysBlockchainWillChangeSociety: {
+      title: 'Jibrel Network - News & Media - 3 Biggest Ways Blockchain Will Change Society',
+      description: 'The potential of blockchain stretches far beyond and high above any of the buzz you hear surrounding cryptocurrency trading profits and initial coin offering deals. The long term, 25-year view of the technology persuasively explains how its core features show the promise to serve,',
+      url: '3-biggest-ways-blockchain-will-change-society.html'
     }
 }
