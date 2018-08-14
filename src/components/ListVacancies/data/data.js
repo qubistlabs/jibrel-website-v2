189fe8251fd5059ee8f381ab905642b187756362
Dragonesis <dragonesis@ya.ru>
2018-08-14 11:54:32 +0300

    Change the sorting of vacancies

--- src/components/ListVacancies/data/data.js
+++ src/components/ListVacancies/data/data.js
@@ -1,29 +1,5 @@
 ListVacancies: [
   {
-    link: 'senior-qa-automation-engineer-ios.html',
-    place: 'Saint-Petersburg, Russia',
-    position: 'Senior QA Automation Engineer (iOS)',
-    descr: 'Senior QA Automation Engineer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. Start-up is fully funded for 3 years of development.',
-  },
-  {
-    link: 'senior-qa-automation-engineer-android.html',
-    place: 'Saint-Petersburg, Russia',
-    position: 'Senior QA Automation Engineer (Android)',
-    descr: 'Senior QA Automation Engineer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. Start-up is fully funded for 3 years of development.',
-  },
-  {
-    link: 'senior-qa-engineer.html',
-    place: 'Saint-Petersburg, Russia',
-    position: 'Senior QA Engineer',
-    descr: 'Senior QA Engineer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. Start-up is fully funded for 3 years of development',
-  },
-  {
-    link: 'senior-mobile-qa-engineer.html',
-    place: 'Saint-Petersburg, Russia',
-    position: 'Senior Mobile QA Engineer',
-    descr: 'Senior Mobile QA Engineer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. Start-up is fully funded for 3 years of development.',
-  },
-  {
     link: 'digital-marketing-analyst.html',
     place: 'New York, USA',
     position: 'Digital Marketing Analyst',
@@ -46,5 +22,29 @@ ListVacancies: [
     place: 'New York, USA',
     position: 'Community Manager',
     descr: 'Jibrel is a blockchain development company, with offices in Zug, Switzerland, New York, US and Saint Petersburg, Russia. Jibrel focuses on building financial technology products for Tier 1 Banks and non-bank financial institutes as well as governments and regulators. Jibrel aims to leverage the latest innovations in distributed ledger and smart contract technology to build the financial networks of the future.',
+  },
+  {
+    link: 'senior-qa-automation-engineer-ios.html',
+    place: 'Saint-Petersburg, Russia',
+    position: 'Senior QA Automation Engineer (iOS)',
+    descr: 'Senior QA Automation Engineer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. Start-up is fully funded for 3 years of development.',
+  },
+  {
+    link: 'senior-qa-automation-engineer-android.html',
+    place: 'Saint-Petersburg, Russia',
+    position: 'Senior QA Automation Engineer (Android)',
+    descr: 'Senior QA Automation Engineer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. Start-up is fully funded for 3 years of development.',
+  },
+  {
+    link: 'senior-qa-engineer.html',
+    place: 'Saint-Petersburg, Russia',
+    position: 'Senior QA Engineer',
+    descr: 'Senior QA Engineer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. Start-up is fully funded for 3 years of development',
+  },
+  {
+    link: 'senior-mobile-qa-engineer.html',
+    place: 'Saint-Petersburg, Russia',
+    position: 'Senior Mobile QA Engineer',
+    descr: 'Senior Mobile QA Engineer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. Start-up is fully funded for 3 years of development.',
   },
 ]
