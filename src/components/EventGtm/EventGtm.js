ad843728ef3ffbff411a3a61c48a37a4288f482b
Dragonesis <dragonesis@ya.ru>
2018-09-10 19:29:56 +0300

    JSITE-56 403, 404, 500 Error pages add

--- src/components/EventGtm/EventGtm.js
+++ src/components/EventGtm/EventGtm.js
@@ -1,21 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import polyfillForEach from '../../assets/js/plugins/polyfillForEach'
 
-const host = window.location.hostname
-const path = window.location.pathname
-const title = document.querySelector('title').textContent
+const mainMenu = document.querySelector('.main-menu')
 
-const mainMenuLinks = document.querySelector('.main-menu').querySelectorAll('.link')
-const menuOpen = document.querySelector('.main-header').querySelector('.js-popup-open')
-const menuClose = document.querySelector('.main-menu').querySelector('.js-popup-close')
-
-// eslint-disable-next-line fp/no-mutating-methods
-window.dataLayer.push({
-  'event': 'virtualPageview',
-  'virtualTitle': title,
-  'virtualUrl': path,
-  'virtualHost': host,
-})
 
 function menuDataSend(category, action, label) {
   // eslint-disable-next-line fp/no-mutating-methods
@@ -29,17 +16,33 @@ function menuDataSend(category, action, label) {
   return true
 }
 
+if (mainMenu) {
+  const host = window.location.hostname
+  const path = window.location.pathname
+  const title = document.querySelector('title').textContent
+  const mainMenuLinks = mainMenu.querySelectorAll('.link')
+  const menuOpen = document.querySelector('.main-header').querySelector('.js-popup-open')
+  const menuClose = mainMenu.querySelector('.js-popup-close')
 
-menuOpen.addEventListener('click', () => {
-  menuDataSend('Menu', 'Open_menu', '')
-})
-menuClose.addEventListener('click', () => {
-  menuDataSend('Menu', 'Close_menu', '')
-})
-
-mainMenuLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    const label = link.textContent
-    menuDataSend('Menu', 'GoTo', label)
+  // eslint-disable-next-line fp/no-mutating-methods
+  window.dataLayer.push({
+    'event': 'virtualPageview',
+    'virtualTitle': title,
+    'virtualUrl': path,
+    'virtualHost': host,
   })
-})
+
+  menuOpen.addEventListener('click', () => {
+    menuDataSend('Menu', 'Open_menu', '')
+  })
+  menuClose.addEventListener('click', () => {
+    menuDataSend('Menu', 'Close_menu', '')
+  })
+
+  mainMenuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      const label = link.textContent
+      menuDataSend('Menu', 'GoTo', label)
+    })
+  })
+}
