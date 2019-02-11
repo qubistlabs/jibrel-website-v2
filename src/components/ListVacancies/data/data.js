70885ca4fdd5e475c0476cc786235b85ce0b41b2
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-02-11 17:29:17 +0300

    Fix vacancies sorting

--- src/components/ListVacancies/data/data.js
+++ src/components/ListVacancies/data/data.js
@@ -6,12 +6,6 @@ ListVacancies: [
     descr: 'DevOps Engineer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. Start-up is fully funded for 3 years of development.',
   },
   {
-    link: 'financial-controller.html',
-    place: 'Dubai, UAE',
-    position: 'Financial Controller',
-    descr: 'Jibrel is a looking for a Financial Controller to join its Dubai office. Jibrel\'s Financial Controller will be responsible for overseeing day-to-day financial and accounting activities across Jibrel operating locations and offices.',
-  },
-  {
     link: 'middle-python-developer.html',
     place: 'Saint-Petersburg, Russia',
     position: 'Middle Python Developer',
@@ -22,5 +16,11 @@ ListVacancies: [
     place: 'Saint-Petersburg, Russia',
     position: 'UX Analyst & Designer',
     descr: 'UI/UX Analyst & Designer for an international blockchain startup that develops tools for Banks and Funds to use blockchain & tokens and comply with the SEC and other regulators. You\'ll need to work on UI/UX of the apps: analyze and develop entirely new features, solve numerous UX issues that arise due to the integration with the blockchain and other complex features, polish UI/UX of the apps, provide specifications for design and development, attest the UX of the apps, improve the apps during all the lifecycle.',
+  },
+  {
+    link: 'financial-controller.html',
+    place: 'Dubai, UAE',
+    position: 'Financial Controller',
+    descr: 'Jibrel is a looking for a Financial Controller to join its Dubai office. Jibrel\'s Financial Controller will be responsible for overseeing day-to-day financial and accounting activities across Jibrel operating locations and offices.',
   },
 ]
