69e488d5cc918f6fbf86d086958d3eca6279a77c
Evgeny Vanchugov <dragonesis@ya.ru>
2019-03-12 15:59:40 +0300

    Update project logo

--- .vuepress/src/components/ProjectLogo/ProjectLogo.vue
+++ .vuepress/src/components/ProjectLogo/ProjectLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-link to='/' class='project-logo' :class='`-${color}`' data-aos="fade-down" data-aos-duration="600" data-aos-delay="150">
+  <router-link :to='this.$localeConfig.path' class='project-logo' :class='`-${colorTheme}`' :data-aos='animationType' data-aos-duration="600" data-aos-delay="150">
     <svg class="icon" width="41.534px" height="45.558px"><use xlink:href="#logo-icon"></use></svg>
     <svg class="text" width="68.829px" height="26.945px"><use xlink:href="#logo-text"></use></svg>
   </router-link>
@@ -9,7 +9,8 @@
 export default {
   name: 'ProjectLogo',
   props: {
-    color: 'color' | 'white'
+    colorTheme: 'colored' | 'white',
+    animationType: 'fade-down' | 'fade-in'
   },
 }
 </script>
