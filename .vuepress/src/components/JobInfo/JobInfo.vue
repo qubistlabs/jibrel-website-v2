571fe099b834137d5f34ec5884f92efbe466c9e3
Evgeny Vanchugov <dragonesis@ya.ru>
2019-03-12 15:39:55 +0300

    Add list vacancies and JobInfo

---
+++ .vuepress/src/components/JobInfo/JobInfo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class='job-info' :class='onVacanciesList ? "-in-vacancies-list" : "-in-vacancy-page"'>
+      <div class='time'>
+        <svg class="icon" width="15px" height="15px"><use xlink:href="#i-clock"></use></svg>
+        {{ time }}
+      </div>
+      <div class='place'>
+        <svg class="icon" width="15px" height="15px"><use xlink:href="#i-pin"></use></svg>
+        {{ place }}
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'JobInfo',
+  props: {
+    time: String,
+    place: String,
+    onVacanciesList: false
+  },
+  
+}
+</script>
+
+
+<style lang="scss">
+  @import './jobInfo.scss';
+</style>
