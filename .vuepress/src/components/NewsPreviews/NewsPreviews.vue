ef09b41423367665bbbde6e1342ae0f1a2f38f44
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-04-25 12:08:13 +0300

    Update aos duration for news

--- .vuepress/src/components/NewsPreviews/NewsPreviews.vue
+++ .vuepress/src/components/NewsPreviews/NewsPreviews.vue
@@ -1,6 +1,6 @@
 <template>
   <div class='news-preview'>
-    <div class='item' data-aos='fade-down' data-aos-duration='300' data-aos-delay='250' v-for='post in posts' :key='post.path'>
+    <div v-for='(post, index) in posts' class='item' data-aos='fade-down' :data-aos-duration='300 + index % 3 * 300' data-aos-delay='250' :key='post.path'>
       <router-link :to='post.path' class='box' :class='isBordered && "-border"'>
         <div class='pic'>
           <img
