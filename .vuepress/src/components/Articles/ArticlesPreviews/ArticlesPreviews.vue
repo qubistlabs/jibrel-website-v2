215fc0d3bf82400b0d4bf49aa14b6616196fcab7
Evgeny Vanchugov <dragonesis@ya.ru>
2019-06-17 13:32:18 +0300

    JSITE-108 Last article in the primary block

--- .vuepress/src/components/Articles/ArticlesPreviews/ArticlesPreviews.vue
+++ .vuepress/src/components/Articles/ArticlesPreviews/ArticlesPreviews.vue
@@ -1,11 +1,19 @@
 <template>
   <div class='articles-preview'>
-    <div v-for='(post, index) in posts' class='item' data-aos='fade-down' :data-aos-duration='300 + index % 3 * 300' data-aos-delay='250' :key='post.path'>
-      <router-link :to='post.path' class='box' :class='isBordered && "-border"'>
+    <div 
+      v-for='(post, index) in posts' 
+      class='item' 
+      :class='setClassToFirstArticle(index)'
+      data-aos='fade-down' 
+      :data-aos-duration='aosDuration(index)' 
+      data-aos-delay='250' 
+      :key='post.path'
+    >
+      <router-link :to='post.path' class='box'>
         <div class='pic'>
           <img
-            :src='`/assets/img/blog/${ post.frontmatter.source.img }.jpg`'
-            :srcset='`/assets/img/blog/${ post.frontmatter.source.img }@2x.jpg 2x`'
+            :src='`/assets/img/blog/${ post.frontmatter.source.img }${postfixImgToFirstArticle(index)}.jpg`'
+            :srcset='`/assets/img/blog/${ post.frontmatter.source.img }${postfixImgToFirstArticle(index)}@2x.jpg 2x`'
             :alt='post.frontmatter.title'
             class='img'
           >
@@ -19,25 +27,49 @@
               {{ post.frontmatter.date | formatDate }}
             </time>
             <router-link :to='getTagUrl(post.path)' class='tag'>{{getTagData(post.path)}}</router-link>
+            <div class='more' v-if='isMainBlogPage && index === 0'><button class='j-button -fill-on-white-bg -h-small'>Read  •  8 min</button></div>
+            <slot />
           </div>
           <h2 class='title'>{{ post.frontmatter.title }}</h2>
           <p class='descr'>{{ post.frontmatter.description }}</p>
         </div>
       </router-link>
+      <ArticlesSubscribe v-if='isMainBlogPage && index === 0' />
     </div>
   </div>
 </template>
 
 <script>
 import EventBus from '@/utils/EventBus';
+import ArticlesSubscribe from './ArticlesSubscribe/ArticlesSubscribe.vue';
 export default {
   name: 'ArticlesPreviews',
-  props: {
-    isBordered: false,
+  components: {
+    ArticlesSubscribe
+  },
+  props: { 
     locale: String,
+    isMainBlogPage: Boolean,
     limit: 0,
   },
   methods: {
+    postfixImgToFirstArticle(index) {
+      if (this.isMainBlogPage) {
+        return index === 0 ? '-full' : ''
+      }
+      return ''
+    },
+    setClassToFirstArticle(index) {
+      if (this.isMainBlogPage) {
+        return index === 0 && '-first-card'
+      }
+    },
+    aosDuration(index) {
+      if (this.isMainBlogPage && index !== 0) {
+        index -= 1
+      }
+      return 300 + index % 3 * 300
+    },
     getTagUrl(path) {
       if (path.indexOf('/blog/how-to-is/') !== -1) {        
         return '/blog/how-to-is/'
@@ -66,10 +98,12 @@ export default {
       const posts = this.$site.pages
         .filter(x => x.path.startsWith(this.$page.path) && !x.frontmatter.index)
         .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+        console.log(posts, this.$page);
 
       if (!this.limit) {
         return posts
       }
+      
       EventBus.$emit('posts-length', posts.length)
       return posts.slice(0, Number(this.limit))
     },
