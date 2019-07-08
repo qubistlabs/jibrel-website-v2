747cc84a95db04460a1df8d2e4b7a79822507d74
Evgeny Vanchugov <dragonesis@ya.ru>
2019-07-08 13:37:35 +0300

    JSITE-90 Setting redirect on browser default language

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,5 +1,36 @@
 import { format } from 'date-fns'
 
+const redirectPath = {
+  init() {
+    if (window.navigator.languages) {
+      return this.processingLanguages(window.navigator.languages)
+    }
+    if (window.navigator.browserLanguage) {
+      return this.switchLanguage(window.navigator.browserLanguage)
+    }
+    if (window.navigator.language) {
+      return this.switchLanguage(window.navigator.language)
+    }
+    return '/en/'
+  },
+  processingLanguages(arr) {   
+    for (var index = 0; index < arr.length; index++) {      
+      switch (arr[index].slice(0, 2)) {
+        case 'en': return '/en/'
+        case 'ko': return '/ko/'
+        default: return '/en/'
+      }
+    }
+  },
+  switchLanguage(lang) {
+    switch (lang.slice(0, 2)) {
+      case 'en': return '/en/'
+      case 'ko': return '/ko/'
+      default: return '/en/'
+    }
+  }
+}
+
 export default ({
   Vue,
   router,
@@ -8,6 +39,6 @@ export default ({
     return format(date, 'MMM. D, YYYY')
   })
   router.addRoutes([
-    { path: '/', redirect: '/en/' },
+    { path: '/', redirect: redirectPath.init()},
   ])
 }
